b9f8cdbc1bedc5c70a00a1049bc32ef71c9adc44
Joseph Rhines <jprhines@gmail.com>
2021-03-07 21:14:01 -0600

    I added the code for the footer into the Footer.js file

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
     return (
-       "test"
+        <footer>Name: Joseph Rhines, Phone Number: 612-715-7890, <a href="jprhines@gmail.com">Email</a> ,<a href="https://github.com/JosephRhines">Github</a> ,<a href="https://www.linkedin.com/in/josephrhines/">Linkedin, </a><a href="https://docs.google.com/document/d/1B8ysi_UfE8RLVN22UiUJlHjxK-bWoozpwMRMxbCEEjA/edit?usp=sharing">Resume</a></footer>
     );
 }
 
