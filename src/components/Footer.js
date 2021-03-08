4f5d1cfd57bf058ac98d9b9c1837af290dfa13e5
Joseph Rhines <jprhines@gmail.com>
2021-03-07 21:55:18 -0600

    I added mailto: in my email link so the default email window pops up

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "../styles/Footer.css"
 
 function Footer() {
     return (
-        <footer>Name: Joseph Rhines, Phone Number: 612-715-7890, <a href="jprhines@gmail.com">Email</a> ,<a href="https://github.com/JosephRhines">Github</a> ,<a href="https://www.linkedin.com/in/josephrhines/">Linkedin, </a><a href="https://docs.google.com/document/d/1B8ysi_UfE8RLVN22UiUJlHjxK-bWoozpwMRMxbCEEjA/edit?usp=sharing">Resume</a></footer>
+        <footer>Name: Joseph Rhines, Phone Number: 612-715-7890, <a href="mailto:jprhines@gmail.com">Email</a> ,<a href="https://github.com/JosephRhines">Github</a> ,<a href="https://www.linkedin.com/in/josephrhines/">Linkedin, </a><a href="https://docs.google.com/document/d/1B8ysi_UfE8RLVN22UiUJlHjxK-bWoozpwMRMxbCEEjA/edit?usp=sharing">Resume</a></footer>
     );
 }
 
