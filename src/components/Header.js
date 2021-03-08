cf2c16536bffe10f531850b3e9d8fb19eb15a2a7
Joseph Rhines <jprhines@gmail.com>
2021-03-07 20:49:21 -0600

    I created a Header component and added code for the Header. I imported the header on the App.js and imported Bootstrap on the index.js

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Header() {
+    return (
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <span class="navbar-brand mb-0 h1">Joseph Rhines</span>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+                aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+           
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+               
+                    <li class="nav-item">
+                        <a class="nav-link" href="portfolio.html">Portfolio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.html">About</a>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+    );
+
+}
+    
+
+    export default Header;
