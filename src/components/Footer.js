96229caa75fda54d70fd6ed78883d67e92a7da91
Joseph Rhines <jprhines@gmail.com>
2021-03-11 19:56:39 -0600

    I adjusted the size of the columns on the portfolio page and added a border. I also deleted some text in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import "../styles/Footer.css"
 
 function Footer() {
     return (
-        <footer>Name: Joseph Rhines, Phone Number: 612-715-7890, <a href="mailto:jprhines@gmail.com">Email</a> ,<a href="https://github.com/JosephRhines">Github</a> ,<a href="https://www.linkedin.com/in/josephrhines/">Linkedin, </a><a href="https://docs.google.com/document/d/1B8ysi_UfE8RLVN22UiUJlHjxK-bWoozpwMRMxbCEEjA/edit?usp=sharing">Resume</a></footer>
+        <footer>(612)-715-7890, <a href="mailto:jprhines@gmail.com">Email</a>, <a href="https://github.com/JosephRhines">Github</a>, <a href="https://www.linkedin.com/in/josephrhines/">Linkedin</a>, <a href="https://docs.google.com/document/d/1B8ysi_UfE8RLVN22UiUJlHjxK-bWoozpwMRMxbCEEjA/edit?usp=sharing">Resume</a></footer>
     );
 }
 
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -19,17 +19,13 @@ function Portfolio() {
 
     
     
-    <section className="row">
-      <div className="col-md-3 border m-1"><img src={nationalparks} width="100%" alt="nationalparks"/><br/><strong>Title: Midwest National Parks</strong><br/><a href="https://yermasog.github.io/Find-a-National-Park-app/" target="_blank" rel="noreferrer">Deployed Link</a><br/><a href="https://github.com/yermasog/Find-a-National-Park-app" target="_blank" rel="noreferrer">Github Repo</a></div>
-      <div className="col-md-3 border m-1"><img src={games} width="100%" alt="Games[i]"/><br/><strong>Title: Games[i]</strong><br/><a href="https://game-i.herokuapp.com/" target="_blank" rel="noreferrer">Deployed Link</a><br/><a href="https://github.com/inqueblot/boardgame_collection" target="_blank" rel="noreferrer">Github Repo</a></div>
-      <div className="col-md-3 border m-1"><img src={burgers} width="100%"  alt="burgers"/><br/><strong>Title: Burger App</strong><br/><a href="https://big-fat-burger.herokuapp.com/" target="_blank" rel="noreferrer">Deployed Link</a><br/><a href="https://github.com/JosephRhines/burger" target="_blank"rel="noreferrer" >Github Repo</a></div>
-     
-    </section>
-    <br/>
-    <section className="row">
-      <div className="col-md-3 border m-1"><img src={quizz} width="100%" alt="quizz"/><br/><strong>Title: Quizz2021</strong><br/><a href="https://josephrhines.github.io/Quiz2021/" target="_blank" rel="noreferrer">Deployed Link</a><br/><a href="https://github.com/JosephRhines/Quiz2021" target="_blank" rel="noreferrer">Github Repo</a></div>
-      <div className="col-md-3 border m-1"><img src={weather} width="100%"  alt="weather"/><br/><strong>Title: WeatherDashboard2021</strong><br/><a href="https://josephrhines.github.io/WeatherDashbord2021/" target="_blank" rel="noreferrer">Deployed Link</a><br/><a href="https://github.com/JosephRhines/WeatherDashbord2021" target="_blank" rel="noreferrer">Github Repo</a></div>
-      <div className="col-md-3 border m-1"><img src={employee} width="100%"  alt="employee-directory"/><br/><strong>Title: Employee-Directory</strong><br/><a href="https://josephrhines.github.io/employee-directory/" target="_blank" rel="noreferrer">Deployed Link</a><br/><a href="https://github.com/JosephRhines/employee-directory" target="_blank" rel="noreferrer">Github Repo</a></div>
+    <section className="row mb-5">
+      <div className="col-md-4 r-border p-1"><img src={nationalparks} width="100%" alt="nationalparks"/><br/><strong>Title: Midwest National Parks</strong><br/><a href="https://yermasog.github.io/Find-a-National-Park-app/" target="_blank" rel="noreferrer">Deployed Link</a><br/><a href="https://github.com/yermasog/Find-a-National-Park-app" target="_blank" rel="noreferrer">Github Repo</a></div>
+      <div className="col-md-4 r-border p-1"><img src={games} width="100%" alt="Games[i]"/><br/><strong>Title: Games[i]</strong><br/><a href="https://game-i.herokuapp.com/" target="_blank" rel="noreferrer">Deployed Link</a><br/><a href="https://github.com/inqueblot/boardgame_collection" target="_blank" rel="noreferrer">Github Repo</a></div>
+      <div className="col-md-4 r-border p-1"><img src={burgers} width="100%"  alt="burgers"/><br/><strong>Title: Burger App</strong><br/><a href="https://big-fat-burger.herokuapp.com/" target="_blank" rel="noreferrer">Deployed Link</a><br/><a href="https://github.com/JosephRhines/burger" target="_blank"rel="noreferrer" >Github Repo</a></div>
+      <div className="col-md-4 r-border p-1"><img src={quizz} width="100%" alt="quizz"/><br/><strong>Title: Quizz2021</strong><br/><a href="https://josephrhines.github.io/Quiz2021/" target="_blank" rel="noreferrer">Deployed Link</a><br/><a href="https://github.com/JosephRhines/Quiz2021" target="_blank" rel="noreferrer">Github Repo</a></div>
+      <div className="col-md-4 r-border p-1"><img src={weather} width="100%"  alt="weather"/><br/><strong>Title: WeatherDashboard2021</strong><br/><a href="https://josephrhines.github.io/WeatherDashbord2021/" target="_blank" rel="noreferrer">Deployed Link</a><br/><a href="https://github.com/JosephRhines/WeatherDashbord2021" target="_blank" rel="noreferrer">Github Repo</a></div>
+      <div className="col-md-4 r-border p-1"><img src={employee} width="100%"  alt="employee-directory"/><br/><strong>Title: Employee-Directory</strong><br/><a href="https://josephrhines.github.io/employee-directory/" target="_blank" rel="noreferrer">Deployed Link</a><br/><a href="https://github.com/JosephRhines/employee-directory" target="_blank" rel="noreferrer">Github Repo</a></div>
      
     </section>
     
