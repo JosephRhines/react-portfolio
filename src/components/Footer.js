220b0fe543fb66b73cce14539fb453b6624b398e
Joseph Rhines <jprhines@gmail.com>
2021-03-22 19:57:12 -0500

    I updated my resume link in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import "../styles/Footer.css"
 
 function Footer() {
     return (
-        <footer>(612)-715-7890, <a href="mailto:jprhines@gmail.com">Email</a>, <a href="https://github.com/JosephRhines">Github</a>, <a href="https://www.linkedin.com/in/josephrhines/">Linkedin</a>, <a href="https://docs.google.com/document/d/1B8ysi_UfE8RLVN22UiUJlHjxK-bWoozpwMRMxbCEEjA/edit?usp=sharing">Resume</a></footer>
+        <footer>(612)-715-7890, <a href="mailto:jprhines@gmail.com">Email</a>, <a href="https://github.com/JosephRhines">Github</a>, <a href="https://www.linkedin.com/in/josephrhines/">Linkedin</a>, <a href="https://drive.google.com/file/d/1Kltz23rUpjQaEKKAgEeGrxXcfsHpMo6R/view?usp=sharing">Resume</a></footer>
     );
 }
 
