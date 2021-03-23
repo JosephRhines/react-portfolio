59ffb2d28642b1faecf84a95062fa83cc07d53b4
Joseph Rhines <jprhines@gmail.com>
2021-03-22 20:07:39 -0500

    I added the code to the footer links to open in a new tab

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import "../styles/Footer.css"
 
 function Footer() {
     return (
-        <footer>(612)-715-7890, <a href="mailto:jprhines@gmail.com">Email</a>, <a href="https://github.com/JosephRhines">Github</a>, <a href="https://www.linkedin.com/in/josephrhines/">Linkedin</a>, <a href="https://drive.google.com/file/d/1Kltz23rUpjQaEKKAgEeGrxXcfsHpMo6R/view?usp=sharing">Resume</a></footer>
+        <footer>(612)-715-7890, <a href="mailto:jprhines@gmail.com">Email</a>, <a href="https://github.com/JosephRhines" target="_blank" rel="noreferrer">Github</a>, <a href="https://www.linkedin.com/in/josephrhines/" target="_blank" rel="noreferrer">Linkedin</a>, <a href="https://drive.google.com/file/d/1Kltz23rUpjQaEKKAgEeGrxXcfsHpMo6R/view?usp=sharing" target="_blank" rel="noreferrer">Resume</a></footer>
     );
 }
 
